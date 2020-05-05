0eefac5b357b71a0ff4cac326d0b57a384947abf
AmrMagdy <a.magdyabdelaal@gmail.com>
2020-05-06 00:31:34 +0400

    init organization service and add it to config server

---
+++ organization-srvc/src/main/java/com/spmia/OrganizationSrvcApplication.java
@@ -0,0 +1,13 @@
+package com.spmia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationSrvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationSrvcApplication.class, args);
+	}
+
+}
